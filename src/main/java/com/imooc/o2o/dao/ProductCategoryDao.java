e961344dcc6d803ef4669951fb94182dfcc8850f
leekunming <522737143@qq.com>
2020-06-12 16:06:09 +0800

    基于java ssm框架校园商铺o2o系统

---
+++ src/main/java/com/imooc/o2o/dao/ProductCategoryDao.java
@@ -0,0 +1,41 @@
+package com.imooc.o2o.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.imooc.o2o.entity.ProductCategory;
+
+public interface ProductCategoryDao {
+	/**
+	 *通过shop id 查询店铺商品类别
+	 *@param long
+	 *return List<ProductCategory> 
+	 *
+	 *
+	 */
+	List<ProductCategory>queryProductCategoryList(long shopId);
+	
+	
+	/**
+	 *批量新增铺商品类别
+	 *@param productCategoryList
+	 *return  
+	 *
+	 *
+	 */
+	
+	int batchInsertProductCategory(List<ProductCategory>productCategoryList);
+
+	
+
+	/**
+	 *删除指定铺商品类别
+	 *@param productCategoryId
+	 *@param shopId
+	 *return effectedNum 
+	 *
+	 *
+	 */
+	int deleteProductCategory(@Param("productCategoryId") long productCategoryId, @Param("shopId") long shopId);
+}
